35be7c8699a4429c8800efe720c6f466e9cf88b4
Dimitry Grebenyuk <46931822+grebenyyk@users.noreply.github.com>
2025-05-03 08:58:07 +0800

    Initial Commit

---
+++ SimpleRSS/SimpleRSSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleRSSApp.swift
+//  SimpleRSS
+//
+//  Created by Dimitry on 03.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct SimpleRSSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleRSSTests/SimpleRSSTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleRSSTests.swift
+//  SimpleRSSTests
+//
+//  Created by Dimitry on 03.05.2025.
+//
+
+import Testing
+@testable import SimpleRSS
+
+struct SimpleRSSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
